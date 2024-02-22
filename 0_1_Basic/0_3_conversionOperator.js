cbc6665fae09145f56d91559cfe3233bac88f366
Soumik Datta <61004995+Soumik77@users.noreply.github.com>
2024-02-22 15:35:27 +0000

    conversion operation done

--- 0_1_Basic/0_3_conversionOperator.js
+++ 0_1_Basic/0_3_conversionOperator.js
@@ -25,8 +25,36 @@ console.log(typeof(booleanIsLoggedIn))
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// operations //
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+1+2+3)
+// console.log(1+2+3+4+"5")
+
+console.log(+true)
+console.log(+"")
+console.log(+"j")
+
+let num1, num2, num3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
 
 
 
