438bcd202d8d7a270ed6be40724e1f982718a70b
Soumik Datta <61004995+Soumik77@users.noreply.github.com>
2024-02-22 07:39:52 +0000

    learn_var_datatypes_done

---
+++ 0_1_Basic/0_1_variable.js
@@ -0,0 +1,22 @@
+const accountId = 144521
+let accountEmail = "soumikdatta229@gmail.com"
+var accountPas = "12345"
+accountCity = "Jaipur"
+let accoutnState;
+
+//accountId = 4 //not allowed
+
+console.log(accountId);
+
+accountEmail = "megha229@gmail.com"
+console.log(accountEmail)
+
+accountPas = "3212"
+console.log(accountPas)
+
+accountCity = "Dhaka"
+console.log(accountCity)
+
+console.table([accountEmail,accountId,accoutnState,accountCity,accountPas])
+
+// prefer not to use var because of issues block scope and functional scope
---
+++ 0_1_Basic/0_2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+
+//alert(3+3)//we are using node.js not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+//primitive datatype
+// number => 2 to the power of 54
+//bigint 
+//string => ""
+//boolean => true
+//null => standalone value
+//undefined =>vale assign hoy 
+//symbol => unique
+
+
+//object
+console.log(typeof "soumik")
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof 4)
+console.log(typeof false)
+
+
+
